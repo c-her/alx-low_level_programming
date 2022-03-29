2c71bde88d0af6754f94c912e9ea38978ac768e0
c.her <cherylmayve@gmail.com>
2022-03-30 00:02:18 +0300

    even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: pointer to char params
+ * @src: pointer to char params
+ * @n: size
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = srs[i];
+	}
+	return (dest);
+}
